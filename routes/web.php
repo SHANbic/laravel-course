ad308e79435028a05d58e5fdb9e3114b37b001b9
SHANbic <supagoof924@hotmail.com>
2020-12-21 19:46:18 +0100

    added routes via multiple kind of controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,25 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'home.index')->name('home.index');
 
-Route::view('/contact', 'home.contact')->name('home.contact');
 
-$posts = [
-    1 => [
-        'title' => 'Intro to Laravel',
-        'content' => 'This is a short intro to Laravel',
-        'is_new' => true,
-        'has_comments' => true
-    ],
-    2 => [
-        'title' => 'Intro to PHP',
-        'content' => 'This is a short intro to PHP',
-        'is_new' => false
-    ]
-];
+Route::get('/', 'HomeController@home')->name('home.index');
 
-Route::get('/posts', function() use ($posts) {
+Route::get('/contact', 'HomeController@contact')->name('home.contact');
+
+Route::get('/single', 'About');
+
+Route::resource('posts', ' PostsController')->only(['index', 'show']);
+
+/* Route::get('/posts', function () use ($posts) {
     //dd(request()->all());
     dd((int)request()->query('page', 1));
     return view('posts.index', ['posts' => $posts]);
@@ -42,41 +35,41 @@ Route::get('/posts/{id}', function ($id) use ($posts) {
     abort_if(!isset($posts[$id]), 404);
     return view('posts.show', ['post' => $posts[$id]]);
 })
-//->where(['id'=>'[0-9]+'])
-->name('posts.show');
+    //->where(['id'=>'[0-9]+'])
+    ->name('posts.show');
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Here are the posts from ' . $daysAgo . ' days ago';
-})->name('posts.recent.index');
+})->name('posts.recent.index'); */
 
-Route::prefix('/fun')->name('fun.')->group(function() use ($posts){
-    Route::get('/responses', function() use ($posts) {
+/* Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
+    Route::get('/responses', function () use ($posts) {
         return response($posts, 201)
-        ->header('Content-Type', 'application/json')
-        ->cookie('MY_COOKIE', 'PIERRE', 360);
+            ->header('Content-Type', 'application/json')
+            ->cookie('MY_COOKIE', 'PIERRE', 360);
     })->name('responses');
-    
-    Route::get('/redirect', function() {
+
+    Route::get('/redirect', function () {
         return redirect('/contact');
     })->name('redirect');
-    
-    Route::get('/back', function() {
+
+    Route::get('/back', function () {
         return back();
     })->name('back');
-    
-    Route::get('/named-route', function() {
-        return redirect()->route('posts.show', ['id'=>1]);
+
+    Route::get('/named-route', function () {
+        return redirect()->route('posts.show', ['id' => 1]);
     })->name('named-route');
-    
-    Route::get('/away', function() {
+
+    Route::get('/away', function () {
         return redirect()->away('https://lovedev.fr');
     })->name('away');
-    
-    Route::get('/json', function() use ($posts) {
+
+    Route::get('/json', function () use ($posts) {
         return response()->json($posts);
     })->name('json');
-    
-    Route::get('/download', function() {
+
+    Route::get('/download', function () {
         return response()->download(public_path('/test.gif'));
     })->name('download');
-});
+}); */
