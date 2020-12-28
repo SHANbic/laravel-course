0e07f50aafd44d458908259b1b8328aa0df875b1
SHANbic <supagoof924@hotmail.com>
2020-12-28 16:36:22 +0100

    added some styling with bootstrap

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,24 +1,13 @@
 @extends('layouts.app')
 
-@section('title', $post['title'])
+@section('title', $post->title)
 
 @section('content')
+<h1>{{ $post->title }}</h1>
+<p>{{ $post->content }}</p>
+<p>Added {{ $post->created_at->diffForHumans() }}</p>
 
-@if($post['is_new'])
-<div>a new blog post</div>
-@else(!$post['is_new'])
-<div>blog post is old
+@if(now()->diffInMinutes($post->created_at) < 5)
+<div class="alert alert-info">New!</div>
 @endif
-
-@unless($post['is_new'])
-<div>it is an old post</div>
-@endunless
-
-<h1>{{ $post['title'] }}</h1>
-<p>{{ $post['content'] }}</p>
-
-@isset($post['has_comments'])
-<div>the post has some comments</div>
-@endisset
-
 @endsection
