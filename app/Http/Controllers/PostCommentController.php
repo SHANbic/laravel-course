723ed0830840d02c26756a945116053986a972d7
SHANbic <supagoof924@hotmail.com>
2021-01-23 19:17:34 +0100

    implemented queues

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -4,10 +4,9 @@ namespace App\Http\Controllers;
 
 use App\BlogPost;
 use App\Http\Requests\StoreComment;
-use App\Mail\CommentPosted;
+use App\Jobs\NotifyUsersPostWasCommented;
+use App\Jobs\ThrottledMail;
 use App\Mail\CommentPostedMarkdown;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 
 class PostCommentController extends Controller
 {
@@ -23,7 +22,16 @@ class PostCommentController extends Controller
             'user_id' => $request->user()->id
         ]);
 
-        Mail::to($post->user)->send(new CommentPostedMarkdown($comment));
+        // Mail::to($post->user)->send(new CommentPostedMarkdown($comment));
+
+        // Mail::to($post->user)->queue(new CommentPostedMarkdown($comment));
+
+        ThrottledMail::dispatch(new CommentPostedMarkdown($comment), $post->user)->onQueue('low');
+
+        NotifyUsersPostWasCommented::dispatch($comment)->onQueue('high');
+
+        // $when = now()->addMinutes(1);
+        // Mail::to($post->user)->later($when, new CommentPostedMarkdown($comment));
 
         return redirect()->back()->withStatus('Comment was created !');
     }
--- app/User.php
+++ app/User.php
@@ -70,4 +70,11 @@ class User extends Authenticatable
             ->has('blogPosts', '>=', 3)
             ->orderBy('blog_posts_count', 'DESC');
     }
+
+    public function scopeThatHasCommentedOnPost(Builder $query, BlogPost $post)
+    {
+        return $query->whereHas('comments', function ($query) use ($post) {
+            return $query->where('commentable_id', '=', $post->id)->where('commentable_type', '=', BlogPost::class);
+        });
+    }
 }
